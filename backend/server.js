ddef902c9af8debf9ca2d4e1c29d6cbe6ec88f7b
FranciscoCTI <franciscofcch@gmail.com>
2025-04-17 13:03:53 -0400

    First commit

---
+++ backend/server.js
@@ -0,0 +1,38 @@
+import express from 'express';
+import { connectDB } from './config/db.js';
+//import Product from './models/product.model.js';
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+//import productRoutes from './routes/product.routes.js';
+import cors from 'cors';
+import path from 'path';
+
+dotenv.config();
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+const __dirname = path.resolve();
+
+app.use(express.json());
+
+app.use(cors());
+
+//app.use('/', serviceProvidersRoutes);
+
+/*
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "/frontend/dist")));
+    app.get(/(.*)/, (req, res) => {
+        res.header('Access-Control-Allow-Origin', '*');
+        res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+    })
+}
+*/
+
+app.listen(PORT, () => {
+    connectDB();
+    console.log(`Server started at http://localhost:${PORT}`);
+    console.log('this is a message from the serveraca');
+});
